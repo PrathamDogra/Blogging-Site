ce98ed151c820a2d0a39c4beb51051ca317f3cf8
Pratham Dogra <dogra.pratham@gmail.com>
2019-04-06 20:29:36 +0530

    Added new model Comment.

--- src/post/models.py
+++ src/post/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from tinymce import HTMLField
 
 User = get_user_model()
 
@@ -17,6 +19,8 @@ class Category(models.Model):
         return self.title
 
 
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
@@ -24,14 +28,37 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     #To count the number of counts
     comment_count = models.IntegerField(default = 0)
+    #To count the comments
+    view_count = models.IntegerField(default = 0)
     #Author field
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
-
+    content = HTMLField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField(default=True)
+    previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null= True)
+    next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null= True)
 
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={
+            'id' :self.id,
+        })
 
+    
+    
     def __str__(self):
         return self.title
+    
+    @property
+    def get_comments(self):
+        return self.comments.all().order_by('-timestamp')
+    
+class Comment(models.Model):
+     user = models.ForeignKey( User, on_delete=models.CASCADE)
+     timestamp = models.DateTimeField(auto_now_add=True)
+     content = models.TextField()
+     post = models.ForeignKey(Post,related_name='comments', on_delete=models.CASCADE)
 
+     def __str__(self):
+        return self.user.username
+    
