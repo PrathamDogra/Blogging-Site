9c1b20c08958750b30e09dabc3ed81773c702097
Pratham Dogra <dogra.pratham@gmail.com>
2019-04-07 13:30:29 +0530

    added get_update and get_delete methods

--- src/post/models.py
+++ src/post/models.py
@@ -39,19 +39,33 @@ class Post(models.Model):
     previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null= True)
     next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null= True)
 
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={
-            'id' :self.id,
-        })
+    
 
     
     
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={
+            'id': self.id
+        })
+    
+    def get_update_url(self):
+        return reverse('post-update', kwargs={
+            'id': self.id
+        })
+
+    def get_delete_url(self):
+        return reverse('post-delete', kwargs={
+            'id': self.id
+        })
+    
     @property
     def get_comments(self):
         return self.comments.all().order_by('-timestamp')
+
+    
     
 class Comment(models.Model):
      user = models.ForeignKey( User, on_delete=models.CASCADE)
